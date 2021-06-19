fef15d1fb5e20f3df5703f3864c290f2df47dd79
Arun Purohit <51073682+arunpurohit1@users.noreply.github.com>
2021-06-19 18:58:38 +0530

    Some Bug Fixed

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -4,32 +4,32 @@ const HttpError = require('../utils/http-error')
 
 module.exports = (req , res , next) => {
 
-    if(req.method == "OPTIONS"){
-        return next()
-    }
-
+     if(req.method == "OPTIONS"){
+        return next();
+     }
     try{
-         token = req.headers.authorization.split(" ")[1];
+         const token = req.headers.authorization.split(" ")[1];
          if(!token){
-             error = new HttpError({"Invalid Token ": 403});
+             error = new HttpError("Invalid Token ", 403);
              return next(error)
          }
          dotenv.config();
-        if(req.headers.role == "User" && req.headers.role == "user"){
+        if(req.headers.role == "User"){
 
             let decodeTokenUser = jwt.verify(token ,process.env.DB_USERKEY);
             req.user = decodeTokenUser;
          }
-        else if(req.headers.role == "Admin" && req.headers.role == "admin"){
-             let decodeTokenUser = jwt.verify(token ,process.env.DB_ADMINKEY);
-             req.admin = decodeTokenUser;
+        else if(req.headers.role == "Admin"){
+             let decodeTokenAdmin = jwt.verify(token ,process.env.DB_ADMINKEY);
+             req.admin = decodeTokenAdmin;
         }
-         
+        
          next();
     }catch(err){
 
-        error = new HttpError({"Authentication Failed Please Try Again Later": 403});
+        error = new HttpError("Authentication Failed Please Try Again Later", 403);
         return next(error)
     }
+    
 
 }
